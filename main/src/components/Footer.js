0633cce6f72641908145a24a6e6414a6d47f1731
AFaidley <alviva.faidley@gmail.com>
2022-12-21 16:52:17 -0700

    Worked on navbar and footer

--- main/src/components/Footer.js
+++ main/src/components/Footer.js
@@ -1,7 +1,5 @@
 const Footer = () => {
-    return (
-        <footer>@REACTiveWeather</footer>
-    )
-}
+  return <footer className='text-muted text-center footer'>@REACTiveWeather- Powered by love and React! ⚛️</footer>;
+};
 
 export default Footer;
--- main/src/pages/Home.js
+++ main/src/pages/Home.js
@@ -1,7 +1,42 @@
 const Home = () => {
     return (
-        <div>
-            <p className="text-align-center">Hi</p>
+        <div id='jumbo-container'>
+        <div className='jumbotron jumbotron-fluid'>
+          <div className='container'>
+            <h1 className='display-4'>REACTiveWeather</h1>
+            <p className='lead'>
+              REACTiveWeather is your all-in-one weather hub!
+            </p>
+          </div>
+          </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+  
         </div>
     )
 }
