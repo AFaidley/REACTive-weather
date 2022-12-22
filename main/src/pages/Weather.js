79d567308eda5a63d7525d28844ecc710086967e
David faidley <david.faidley23@gmail.com>
2022-12-21 17:19:14 -0700

    added code to weather.js

--- main/src/pages/Weather.js
+++ main/src/pages/Weather.js
@@ -1,9 +1,15 @@
+import { Button} from 'react-bootstrap';
+
+function weatherSearch() {
+};
+
 const Weather = () => {
     return (
         <div>
             <p>Welcome to Weather</p>
+            <Button onClick={weatherSearch()} />
         </div>
     )
-}
+};
 
 export default Weather;
