2252c0fb8cfc63af1fd1722b4b9eed543c6ba72d
AFaidley <alviva.faidley@gmail.com>
2023-01-04 17:11:15 -0700

    Configured moment and added sunrise and sunset

--- main/src/pages/WeatherPage.js
+++ main/src/pages/WeatherPage.js
@@ -1,25 +1,47 @@
 import React from 'react';
+import moment from 'moment';
+
+const refresh = () => {
+  window.location.reload();
+};
 
 const WeatherCard = ({ weatherData }) => (
   <div className='flex-container'>
-
-  <div className='card bg-info'>
-    <div className='card-header text-center placeName'>{weatherData.name}</div>
-    <ul className='list-group list-group-flush'>
-      <li className='list-group-item'>
-        Temperature: {weatherData.main.temp}°F
-      </li>
-      <li className='list-group-item'>
-        Feels Like: {weatherData.main.feels_like}°F
-      </li>
-      <li className='list-group-item'>
-        Humidity: {weatherData.main.humidity}%
-      </li>
-      <li className='list-group-item'>
-        Pressure: {weatherData.main.pressure} hPa
-      </li>
-    </ul>
-  </div>
+    <div className='card bg-info'>
+      <div className='card-header text-center placeName'>
+        {weatherData.name}
+      </div>
+      <div className='card-header text-center placeName'>
+        Date: {moment().format('dddd')}, {moment().format('LL')}
+      </div>
+      <ul className='list-group list-group-flush'>
+        <div className='btn-container'>
+        <button className='button refresh bg-info' onClick={refresh}>
+          🔄{' '}
+        </button>
+        </div>
+        <li className='list-group-item'>
+          Temperature: {weatherData.main.temp}°F
+        </li>
+        <li className='list-group-item'>
+          Feels Like: {weatherData.main.feels_like}°F
+        </li>
+        <li className='list-group-item'>
+          Humidity: {weatherData.main.humidity}%
+        </li>
+        <li className='list-group-item'>
+          Pressure: {weatherData.main.pressure} hPa
+        </li>
+        <li className='list-group-item'>
+          Sunrise:{' '}
+          {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-US')}
+        </li>
+        <li className='list-group-item'>
+          Sunset:{' '}
+          {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-US')}
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
