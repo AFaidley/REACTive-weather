36a989eda24a20454c74d0fff28f7fb523efe432
AFaidley <alviva.faidley@gmail.com>
2023-01-20 20:25:19 -0700

    AQI populating to page and styled

--- main/src/pages/AirQuality.js
+++ main/src/pages/AirQuality.js
@@ -1,39 +1,40 @@
-// const airQualityAPIkey = '4929c896-1465-4dd5-927c-6506a0034f03';
 import React from 'react';
 import moment from 'moment';
 
-
-const AirQuality = ({airData}) => {
-  <div className='flex-container'>
-    <div className='card bg-info'>
-      <div className='card-header text-center placeName'>
-        {airData.city}
+const AirQualityCard = ({ airData }) => {
+  return (
+    <div className='flex-container'>
+      <div className='card bg-info'>
+        <div className='card-header text-center placeName'>
+          {airData.data.city}, {airData.data.country}
+        </div>
+        <div className='card-header text-center placeName'>
+          Date: {moment().format('dddd')}, {moment().format('LL')}
+        </div>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            Air Quality: {airData.data.current.pollution.aqius}
+          </li>
+          <li className='list-group-item'>
+            Air Quality Index:
+            <br></br>
+            <span id='good'>0-50</span> = Good
+            <br></br>
+            <span id='moderate'>51-100</span> = Moderate
+            <br></br>
+            <span id='sensitive'>101-150</span> = Unhealthy for sensitive groups
+            <br></br>
+            <span id='unhealthy'>151-200</span> = Unhealthy
+            <br></br>
+            <span id='very-unhealthy'>201-300</span> = Very Unhealthy
+            <br></br>
+            <span id='hazardous'>301+</span> = Hazardous
+          </li>
+        </ul>
       </div>
-      <div className='card-header text-center placeName'>
-        Date: {moment().format('dddd')}, {moment().format('LL')}
-      </div>
-      {/* <ul className='list-group list-group-flush'>
-        <li className='list-group-item'>
-          Temperature: {airData.main.temp}°F
-        </li>
-       </ul> */}
     </div>
-  </div>
+  );
+  //  return <div>Testerific</div>;
 };
 
-export default AirQuality;
-
-// const getGeolocation = () => {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(function(position){
-//             lat1 = position.coords.latitude;
-//             lon1 = position.coords.longitude;
-//         });
-//     }
-// };
-
-// getGeolocation();
-
-// export const getAirQuality = (cityName, stateInput, airQualityAPIkey) => {
-//     return fetch(`https://api.airvisual.com/v2/city?city=${cityName}&state=${stateInput}&country=USA&key=${airQualityAPIkey}`);
-//   };
+export default AirQualityCard;
