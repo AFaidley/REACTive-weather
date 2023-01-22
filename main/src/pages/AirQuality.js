34b28a5bab7fa08532e64c71a0cf33cc668f7d36
AFaidley <alviva.faidley@gmail.com>
2023-01-21 20:54:11 -0700

    Tried to dynamically update aqi color- might have to use state to update though

--- main/src/pages/AirQuality.js
+++ main/src/pages/AirQuality.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import moment from 'moment';
+const aqiResult = document.getElementById('aqiResult');
 
 const AirQualityCard = ({ airData }) => {
+  let airQuality = airData.data.current.pollution.aqius;
+  if (airQuality <= 50) {
+    aqiResult.setAttribute('id', 'good');
+  } else if (airQuality > 50 && airQuality <= 100) {
+    aqiResult.setAttribute('id', 'moderate');
+  } else if (airQuality > 100 && airQuality <= 150) {
+    aqiResult.setAttribute('id', 'sensitive');
+  } else if (airQuality > 150 && airQuality <= 200) {
+    aqiResult.setAttribute('id', 'unhealthy');
+  } else {
+    aqiResult.setAttribute('id', 'very-unhealthy');
+  }
   return (
     <div className='flex-container'>
       <div className='card bg-info'>
@@ -13,7 +26,7 @@ const AirQualityCard = ({ airData }) => {
         </div>
         <ul className='list-group list-group-flush'>
           <li className='list-group-item'>
-            Air Quality: {airData.data.current.pollution.aqius}
+            Air Quality: <span id='good'>{airData.data.current.pollution.aqius}</span>
           </li>
           <li className='list-group-item'>
             Air Quality Index:
